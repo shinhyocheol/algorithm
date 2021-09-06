7d77c2b43bf5d8646f1adf2291f3c86cc8db370d
shinhyocheol <zzzskssk1@naver.com>
2021-09-06 23:16:00 +0900

    2021.09.06(월)  * 커밋 정리 및 최신화

---
+++ src/programmers/MaxNum.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class MaxNum {
+    public static void main(String[] args) {
+        int[] numbers = {6, 10, 10};
+
+        String answer = "";
+
+        String[] result = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            result[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(result, (before, after) -> (after + before).compareTo(before + after));
+
+        if (result[0].equals("0")) {
+            answer = "0";
+        }
+
+        for (int i = 0; i<result.length; i++) {
+            answer += result[i];
+        }
+
+    }
+}
