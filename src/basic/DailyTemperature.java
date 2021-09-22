38b0f699b2e11095aadccb643aedd1456ef3bdc7
shinhyocheol <zzzskssk1@naver.com>
2021-09-23 02:40:57 +0900

    2021.09.23(목)
     * 코딩테스트 연습 오타 수정

--- src/basic/DailyTemperature.java
+++ src/basic/DailyTemperature.java
@@ -38,7 +38,7 @@ public class DailyTemperature {
         System.out.println();
         for (int i=0; i<result.length; i++) {
             System.out.print(result[i] + " ");
-//        }
+        }
         System.out.println();
     }
 
