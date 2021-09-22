a27fa20e9c47020904209be671c16cf5c8c910df
shinhyocheol <zzzskssk1@naver.com>
2021-09-23 02:40:17 +0900

    2021.09.23(목)  * 코딩테스트 연습

---
+++ src/basic/DailyTemperature.java
@@ -0,0 +1,64 @@
+package basic;
+
+import java.util.Stack;
+
+/**
+ *
+ * @문제설명 : 일일온도를 나타내는 int 배열 temperatures {73, 74, 75, 71, 69, 72, 76, 73} 가 주어집니다.
+ *          더 따뜻한 날씨의 날을 얻기위해 해당날짜 이후에 기다려야하는 날짜의 수를 배열로 리턴하세요.
+ *
+ * @문제조건 : 더 따뜻한 날이 오지 않는다면 0을 리턴할 것
+ * @문제분석 :
+ *      1. 73에서는 1일 후 74
+ *      2. 74에서는 1일 후 75
+ *      3. 75에서는 4일 후 76
+ *      4. 71에서는 2일 후 72
+ *      5. 69에서는 1일 후 72
+ *      6. 72에서는 1일 후 76
+ *      7. 76에서는 더 따뜻해 질 날이 오지 않으므로 0
+ *      8. 73에서는 더 따뜻해 질 날이 오지 않으므로 0
+ *
+ * @시간복잡도 : O(N)
+ *  대상 : int[] temperatures
+ *  이유 : for문 실행 0 -> len까지, while문에서는 스택에 있는 값과 바로 찾을 수도 있습니다. 최악의 경우는 인덱스 끝까지 경우도 발생하지만 이 경우는 O(n+n)의 값 그래서 결론은 O(n) 입니다.
+ *
+ * @공간복잡도 : O(n)
+ *  대상 : int[] result = new int[len]
+ *  이유 : for문 실행
+ */
+public class DailyTemperature {
+
+    public static void main(String[] args) {
+
+        DailyTemperature a = new DailyTemperature();
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+
+        int[] result = a.slove(temperatures);
+
+        System.out.println();
+        for (int i=0; i<result.length; i++) {
+            System.out.print(result[i] + " ");
+//        }
+        System.out.println();
+    }
+
+    public int[] slove(int[] temperatures) {
+
+        int len = temperatures.length;
+        int[] result = new int[len];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<len; i++) {
+
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/basic/TwoSum.java
@@ -0,0 +1,79 @@
+package basic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @문제설명 : 정수의 배열 nums와 정수 target이 주어집니다. 배열 nums에서 두 숫자의 값을 더하여 target이 주어집니다.
+ *      배열 nums에서 두 숫자의 값을 더하여 target 값과 동일할 경우 두 숫자의 인덱스를 리턴합니다. 각 입력에 정확히 하나의 솔루션이 있다고 가정하며,
+ *      동일한 요소를 두 번 사용할 수 없습니다.
+ * @문제조건 : 시간복잡도 O(n)으로 수행하세요.
+ * @풀이설명 :
+ * @문제분석 :
+ *       1. for문을 돌려서 target과 비교
+ *       2. 16-2=11
+ *       3. Map(숫자, 방번호)
+ *       4. 방번호만 리턴한다 int[]
+ *
+ *
+ * @시간복잡도 : O(N) -> 문제 조건자체가 O(N)으로 수행해야 하는 문제임
+ *  대상 : int[] nums
+ *  이유 : for문 실행 0 -> len까지
+ *
+ * @공간복잡도 : O(n)
+ *  대상 : Map<Integer, Integer> map = new HashMap<>();
+ *  이유 : for 문을 실행하면서 map.put
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        TwoSum a = new TwoSum();
+        int[] nums = {2, 6, 11, 14};
+        int target = 16;
+
+        int[] result = a.slove_2(nums, target);
+
+        System.out.print("result : ");
+        for (int i=0; i<result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+
+    }
+
+    // O(N^2)
+    public int[] slove(int[] nums, int target) {
+        int len = nums.length;
+
+        for (int i=0; i<len; i++) {
+            for (int j=i+1; j<len; j++) {
+                if (target == nums[i] + nums[j]) {
+                    return new int[] {i+1, j+1};
+                }
+            }
+        }
+
+        return new int[] {0, 0};
+    }
+
+    // O(N)
+    public int[] slove_2(int[] nums, int target) {
+        // 1.ds
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // 2.for
+        for (int i=0; i<nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int val = map.get(nums[i]); // 14
+                result[0] = val + 1;
+                result[1] = i + 1;
+            } else {
+                map.put(target-nums[i], i);
+            }
+        }
+        return result;
+    }
+
+}
