db5a259e300efee323460a31d50e48e1296a10f8
shinhyocheol <zzzskssk1@naver.com>
2021-09-27 16:52:19 +0900

    2021.09.27(월)
     * 코딩테스트 연습
       - 프로그래머스 데브매칭 기출문제 연습 : K_index

---
+++ src/dataStructure/ArrayListEx.java
@@ -0,0 +1,16 @@
+package dataStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx {
+
+    public static void main(String[] args) {
+
+        List<String> arrayList = new ArrayList<>();
+
+//        arrayList.add();
+
+    }
+
+}
